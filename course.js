576d5e8361a33a9b1f24932c542ca209ef6e4f54
BOOBESH776 <cboobesh776@gmail.com>
2026-03-07 12:09:42 +0530

    add comment line and display the array length and course and then push that

--- course.js
+++ course.js
@@ -1,3 +1,12 @@
 
+// Add New Course 
+
 let course = ["java", "python", "React"];
 
+console.log(course);
+console.log(course.length);
+
+course.push("javaScript");
+console.log(course);
+
+console.log(course.length);
