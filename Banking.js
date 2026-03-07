ae859e8d4c8357043e4c85a070713f3bd9df03f1
BOOBESH776 <cboobesh776@gmail.com>
2026-03-07 12:51:35 +0530

    banking transcation: use Loop + Conditional ,Positive numbers = deposits, negative = withdrawals.

--- Banking.js
+++ Banking.js
@@ -1,3 +1,14 @@
 // Banking Transaction 
 
-let money =  [2000, -500, -1200, 3000, -700];
+let money = [2000, -500, -1200, 3000, -700];
+var tot = 0;
+for (let i=0 ; i<money.length; i++){
+    if(money[i] > 0){
+        console.log("Deposit:"+money[i]);
+    }
+    else{
+        console.log("withdrawals:"+ money[i]);
+    }
+    tot+=money[i];
+}
+console.log("final amount:"+tot);
